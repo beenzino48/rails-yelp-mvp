a0cbcfed9da9344b30a797ab2b949f8b75055f92
hamish liu <hamish.mc.liu@gmail.com>
2022-07-05 22:05:21 +0900

    partly finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # rails acive records
+  # A visitor can see the list of all restaurants.
+  # this creates the seven crud things but only selects index
+  resources :restaurants, only: [:index, :new, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
