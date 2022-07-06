51fca5fa1dbe57671748374a3093a8fe1194d4f1
hamish liu <hamish.mc.liu@gmail.com>
2022-07-06 18:00:28 +0900

    finished but with no styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  # before_action :find_restaurant
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
@@ -10,8 +11,11 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     # This is the review of a restaurant
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
