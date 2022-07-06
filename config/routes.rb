d7122850aecad4ab5f5b9b8aca1fdc9ab38b5817
hamish liu <hamish.mc.liu@gmail.com>
2022-07-06 17:15:58 +0900

    nearly finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # rails acive records
   # A visitor can see the list of all restaurants.
   # this creates the seven crud things but only selects index
-  resources :restaurants, only: [:index, :new, :show] do
+  resources :restaurants, only: [:index, :new, :show, :create] do
     resources :reviews, only: [:new, :create]
   end
 end
